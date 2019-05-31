e2d7c98f98db1bf4fa3c22fa431979b6cb6aa12b
kruhol <n.kruhol@gmail.com>
2019-05-31 19:51:42 +0300

    change some dependencies for package.json

--- src/app.js
+++ src/app.js
@@ -9,8 +9,6 @@ require('ngstorage');
 require('bootstrap/dist/css/bootstrap.css');
 require('bootstrap/dist/js/bootstrap.js');
 require('font-awesome/css/font-awesome.css')
-require('popper.js/dist/popper.js');
-require('jquery/dist/jquery.js');
 
 // util
 function importAll(r) {
@@ -23,14 +21,16 @@ angular.module('app', ['ui.router', 'ngStorage']);
 // app deps
 require('./constants.js');
 
-function addAuthGuard($rootScope, $location, $sessionStorage){
-  // redirect to login page if not logged in and trying to access a restricted page
-  $rootScope.$on('$locationChangeStart', function (event, next, current) {
-    if ( $location.path() != '/login' && !$sessionStorage.user ) {
-      $location.path('/login');
-    }
-  });
-}
+// function addAuthGuard($rootScope, $location, $sessionStorage){
+//   // redirect to login page if not logged in and trying to access a restricted page
+//   $rootScope.$on('$locationChangeStart', function (event, next, current) {
+//     // if ( $location.path() != '/login' && !$sessionStorage.user ) {
+//     //   $location.path('/login');
+//     // }
+
+//     $location.path('/home');
+//   });
+// }
 
 function defaultRoute($urlRouterProvider){
   $urlRouterProvider.otherwise('/home')
@@ -47,12 +47,12 @@ importAll(require.context('./filters', false, /\.js$/));
 importAll(require.context('./directives', false, /\.js$/))
 
 // routers
-importAll(require.context('./login', false, /.js$/))
+// importAll(require.context('./login', false, /.js$/))
 importAll(require.context('./home', true, /.js$/))
 
 angular.module('app')
   .config(['$urlRouterProvider', defaultRoute])
-  .run(['$rootScope', '$location', '$sessionStorage', addAuthGuard])
+  // .run(['$rootScope', '$location', '$sessionStorage', addAuthGuard])
 /*
 // https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
 // debugging
